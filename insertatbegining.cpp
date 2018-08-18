4bd1cc1fe61fcb49407a1184c9a6a93730efdaa8
codefreaksubham <subhamengg99@gmail.com>
2018-08-18 23:50:34 +0530

    initial commit

---
+++ insertatbegining.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<memory.h>
+
+using namespace std;
+
+typedef struct node
+{
+int info;
+struct node *next;
+}node;
+
+  node* head = NULL;
+
+void insertatbeginning(int item)
+{
+	node *ptr;
+	ptr=(node*) malloc(sizeof(node));
+	ptr->info=item;
+	if(head==NULL)
+	{ ptr->next=NULL;
+	  head=ptr;}
+	else
+	 {ptr->next=head;
+      head=ptr;}
+}
+
+void showlist()
+{
+  node *ptr;
+  ptr = head;
+  while(ptr->next!=NULL)
+  {
+    cout<<ptr->info<<endl;
+    ptr=ptr->next;
+  }
+
+}
+
+int main()
+{
+  showlist();
+  insertatbeginning(2);
+  insertatbeginning(3);
+  showlist();
+return 0;
+}
+
---
+++ insertatend.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+typedef struct node
+{
+  int data;
+  struct node* link;
+}node;
+
+void insertatend(node **head,int value)
+{
+  node* newnode=(node*)malloc(sizeof(node));
+  newnode->link==NULL;
+  if(*head==NULL)
+  {
+    *head=newnode;
+    newnode->data=value;
+  }
+  else
+  {
+    node* ptr=*head;
+    while(ptr->link!=NULL)
+    {
+     ptr=ptr->link;
+    }
+    ptr->link=newnode;
+    newnode->data=value;
+  }
+}
+void showlist(node* head)
+{
+    node *ptr;
+    ptr = head;
+  while(ptr!=NULL)
+  {
+    cout<<ptr->data<<endl;
+    ptr=ptr->link;
+  }
+}
+
+
+int main()
+{
+    node* head=NULL;
+    insertatend(&head,1);
+    insertatend(&head,2);
+    insertatend(&head,3);
+    showlist(head);
+    return 0;
+}
